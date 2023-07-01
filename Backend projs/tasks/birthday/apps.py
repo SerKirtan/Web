409888bf912eb43941423a804d90199cd827fea1
Kirtan Patel <82870418+SerKirtan@users.noreply.github.com>
2023-07-01 11:57:50 +0530

    Delete Backend projs/tasks directory

--- Backend projs/tasks/birthday/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class BirthdayConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'birthday'
--- Backend projs/tasks/hello/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.index, name="index"),
-    path("kirtan", views.kirtan, name="kirtan"),
-    path("<str:name>", views.greet, name="greet"),
-]
